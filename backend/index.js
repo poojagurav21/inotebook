2821f87f874bba17e233a14c36e4ed9d19b1d875
Pooja Gurav <pooja.gurav@waiin.com>
2024-06-11 12:26:49 +0530

    api creation

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json());
+//available routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
 app.get('/', (req, res) => {
   res.send('Hello Pooja!')
 })
